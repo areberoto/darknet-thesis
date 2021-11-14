6c797f63eb34fd6561b7174febbd5ce2b3cc2a13
Alberto <alberto.alvarez.96@outlook.com>
2021-11-14 14:51:57 -0600

    Added new and modified files to thesis-branch

---
+++ scripts/gen_yolo_train_labels.sh
@@ -0,0 +1,4 @@
+python3 gen_coco_annotations.py \
+--annotations /home/alberto/darknet/coco/annotations/instances_train2017.json \
+--images /home/alberto/darknet/coco/images/train2017/ \
+--labels /home/alberto/darknet/coco/labels/train2017/
---
+++ scripts/gen_yolo_val_labels.sh
@@ -0,0 +1,4 @@
+python3 gen_coco_annotations.py \
+--annotations /home/alberto/darknet/coco/annotations/instances_val2017.json \
+--images /home/alberto/darknet/coco/images/val2017/ \
+--labels /home/alberto/darknet/coco/labels/val2017/
